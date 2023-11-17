5731b0376b38d14f865841548d875ae2b0af47b4
Rhodinium <127902502+Rhodiniium@users.noreply.github.com>
2023-11-17 17:36:01 +0000

    Correct casino MLO

---
+++ [stream]/[defaultmaps]/qb_casinomisc/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'NoProblem Casino'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files{
+    "peds.meta",
+}
+
+data_file "PED_METADATA_FILE" "peds.meta"
