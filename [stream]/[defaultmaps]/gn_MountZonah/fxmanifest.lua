db7096583d865e0dabaa8d15ac84a350f21457cb
SoulSp34r <davenbancroft@live.co.uk>
2023-12-15 20:11:16 +0000

    udp(MountZonah) New Hospital

---
+++ [stream]/[defaultmaps]/gn_MountZonah/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+file 'gusepe_timecycle_mods_1.xml'
+file 'timecycle_mods_1.xml'
+data_file 'TIMECYCLEMOD_FILE' 'gusepe_timecycle_mods_1.xml'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_coin_op.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_02.ytyp'
+data_file 'TIMECYCLEMOD_FILE' 'timecycle_mods_1.xml'
